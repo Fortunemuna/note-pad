534ac4398a277fff9fd307ee09da8c9b09840cfb
Dev Muna <166520002+Fortunemuna@users.noreply.github.com>
2024-06-10 10:11:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p")
     let img = document.createElement("img");
@@ -9,4 +18,26 @@ createBtn.addEventListener("click", () => {
     inputBox.setAttribute("contenteditable", "true");
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "p"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage(); 
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertlineBreak");
+        event.preventDefault();
+    }
 })
